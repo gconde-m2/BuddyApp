e55fab266bf6170f5973ba66e67f161befc54a5c
Guira <guira@Guiras-MacBook-Pro.local>
2020-10-15 12:37:33 +0200

    pepe

--- client/src/components/pages/Index/Index.js
+++ client/src/components/pages/Index/Index.js
@@ -6,7 +6,7 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Carousel from 'react-bootstrap/Carousel'
-//
+
 import MyComponent from './Vivus'
 
 import './index.css'
